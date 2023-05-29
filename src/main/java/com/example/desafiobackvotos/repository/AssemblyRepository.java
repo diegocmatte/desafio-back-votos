4a38ab8e74ee90a9ac8fa7b4f5cb7a5f49cef06d
Diego Matté <diegocmatte@gmail.com>
2023-05-29 02:36:17 -0300

    changing spring version and fixing swagger

--- src/main/java/com/example/desafiobackvotos/repository/AssemblyRepository.java
+++ src/main/java/com/example/desafiobackvotos/repository/AssemblyRepository.java
@@ -1,6 +1,7 @@
 package com.example.desafiobackvotos.repository;
 
 import com.example.desafiobackvotos.domain.Agenda;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/example/desafiobackvotos/restclient/RestClient.java
+++ src/main/java/com/example/desafiobackvotos/restclient/RestClient.java
@@ -3,17 +3,16 @@ package com.example.desafiobackvotos.restclient;
 import com.example.desafiobackvotos.exception.NoDocumentFoundException;
 import com.example.desafiobackvotos.restclient.response.UserResponse;
 import com.example.desafiobackvotos.util.Constants;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
-import java.util.Objects;
 
 @Component
 public class RestClient {
